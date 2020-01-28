838fdd61489f0d1afe8b1ae260ebbbfcdf9df90e
Maya <60146935+DinosaurPower@users.noreply.github.com>
2020-01-28 11:35:27 -0500

    Add files via upload

---
+++ week2/sketch.js
@@ -0,0 +1,48 @@
+function setup() {
+  createCanvas(810, 810);
+}
+
+//will continually run
+function draw() {
+  background(220);
+  background("#bb0000");
+  //ellipse(x position, y position, width, height);
+  
+  strokeWeight(10);
+    fill("#cc0000");
+   rect(90, 90, 400, 400);
+     fill("#000000");
+  rect(90, 490, 200, 200);
+    fill("#cccccc");
+  rect(290, 490, 200, 100);
+    fill("#cccccc");
+  rect(290, 590, 200, 100);
+     fill("#000000");
+  rect(290, 690, 200, 100);
+     fill("#cccccc");
+  rect(90, 690, 200, 200);
+     fill("#0000bb");
+  rect(490, 590, 300, 200);
+     fill("#eeeeee");
+  rect(490, 490, 300, 100);
+     fill("#eeeeee");
+   rect(490, 290, 150, 200);
+     fill("#eeeeee");
+  rect(640, 290, 150, 200);
+     fill("#ffcc00");
+  rect(490, 90, 300, 200);
+     fill("#eeeeee");
+  rect(-5, 90, 95, 200);
+     fill("#cccccc");
+   rect(-5, 290, 95, 300);
+     fill("#ffcc00");
+  rect(-5, 590, 95, 300);
+     fill("#ffffff");
+  rect(290, 760, 600, 100);
+  rect(-5,-7, 495, 100);
+    rect(-5,-7,195, 100);
+     fill("#ffcc00");
+    rect(490,-7,300, 100);
+  fill("#eeeeee");
+    rect(790,-7,100, 600);
+}
