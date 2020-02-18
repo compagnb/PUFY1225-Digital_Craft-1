d29c5bdf3e851fc71f0f72d7d5149e45d3a01ac9
Maya <60146935+DinosaurPower@users.noreply.github.com>
2020-02-18 09:14:03 -0500

    Add files via upload

---
+++ Epilepsy/sketch.js
@@ -0,0 +1,14 @@
+let a, i;
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(220);
+  for(var i= 0; i < width; i+=40){
+  for (var a= 0; a < height; a+=40){
+  rect(a, i, random(10, 250), random(10, 250));
+    fill(random(0, 255), random(0, 255), random(0, 255));
+       }
+  }
+}
